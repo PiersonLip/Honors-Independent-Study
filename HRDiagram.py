bc84b9c262e59ac560347f8161891ef9f3ae2566
Pierson Lipschultz <piersonlipschultz@gmail.com>
2025-04-13 23:42:54 -0500

    Updated HR Diagram code, generated lots of new figues, corrected various incorrect aspects of paper

--- HRDiagram.py
+++ HRDiagram.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 # functions for making all of the graphs, saves them all with file names corrpsonding to the var used and conditons
 
 # color_map_HR (variable, name_of_var, Log1 = 'F', S_R = 'T', minR = 1.5, maxR = 6.5)
@@ -9,23 +12,23 @@
 # minR = minimum y limit
 # maxR = max y limit
 
-import matplotlib.pyplot as plt
-import numpy as np
-def color_map_HR (DB, variable, name_of_var, title, saveLoc, Log1 = 'F', S_R = 'T', minR = 1.5, maxR = 6.5, ylimit = 'T', style = 'dark_background'):
-    plt.figure(figsize = (8,8))
+def color_map_HR (DB, variable, name_of_var, title, saveLoc, exampleLum = 0, exampleTemp = 0, examplePoint = 'F', Log1 = 'F', S_R = 'T', minR = 1.5, maxR = 6.5, ylimit = 'T', style = 'dark_background'):
+    plt.style.use(style) #graph style
+    fig, ax = plt.subplots(figsize = (8,8))  # create figure and axis
+    ax.grid(True)  # turn on grid
+    ax.set_axisbelow(True)  # make grid lines draw below plotted points
+    ax.yaxis.grid(color='gray', linestyle='dashed')  # customize grid style
+    
     cm = plt.colormaps['RdYlBu']  #This is the color map for the stars
-
+    
     if S_R == 'T':
         r_dot= 10 ** DB['S2_log_R']
         plt.suptitle("Size of dot corresponds to Donor radius", fontsize=10, family="monospace", color='.5')
     else:
         r_dot = S_R
-        
-    plt.style.use(style) #graph style
 
     # assings axis
     Temp = np.log10((((10 ** DB['S2_log_L'])/(10 ** DB['S2_log_R'])**2)**.25) * 5772)
-  
     Lum = DB['S2_log_L']
 
     # binds the color of the scatter points to the x location (temp) of the star
@@ -36,33 +39,43 @@ def color_map_HR (DB, variable, name_of_var, title, saveLoc, Log1 = 'F', S_R = '
     colors = c
 
     # labels
-    plt.title(title)
-    plt.xlabel(r'$log_{10}$ Temperature [K]')
-    plt.ylabel(r'$log_{10}$ Luminosity [$L_{\odot}$]')
+    ax.set_title(title)
+    ax.set_xlabel(r'$log_{10}$ Temperature [K]')
+    ax.set_ylabel(r'$log_{10}$ Luminosity [$L_{\odot}$]')
     
     if ylimit == 'T':
-        plt.ylim(minR, maxR)
+        ax.set_ylim(minR, maxR)
     # axis scaling
     #plt.xscale('log')
     #plt.yscale('log')
 
     #scatter points. "cmap" is setting the colormap to use, "c" is setting the color itself (based on location), "s" is setting the size of the dot based off of star radii
-    plt.scatter(Temp, Lum, cmap = cm, c = colors, s = r_dot)
+    scatter = ax.scatter(Temp, Lum, cmap = cm, c = colors, s = r_dot)
+
     #set limits of graph (tada much better funciton then redefining limits manually)
-    plt.gca().invert_xaxis()
+    ax.invert_xaxis()
 
     # color bar stuff
-    cbar = plt.colorbar(orientation='vertical', label = name_of_var) #sets the colorbar to be horizontal instead of default
+    cbar = fig.colorbar(scatter, ax=ax, orientation='vertical')  # <-- link colorbar to that scatter
+    cbar.set_label(name_of_var)  # <-- set the colorbar label separately
     
     #cbar.ax.invert_xaxis() #invert the color bar  (to match the inverted x scaling)
     #cbar.set_ticks([np.min(c),numpy.median,np.max(c),]) # remove the annoying ticks and labels
+
+    if examplePoint == 'T':
+        scatter = ax.scatter(np.log10(exampleTemp), np.log10(exampleLum), color = 'black', s = 100, marker = '*')
+
+
     if S_R == 'T':
         S_R_STR = str(S_R)
     else:
         S_R_T = int(S_R)
         S_R_STR = str(S_R_T) 
+
     F_Name = saveLoc, title, name_of_var, 'log10', Log1, 'star radius', S_R_STR
     F_Name_str = ' '.join(F_Name)
     print(F_Name_str)
+    
     plt.savefig(F_Name_str, dpi=200)
     plt.style.use('default')
+    plt.close()
